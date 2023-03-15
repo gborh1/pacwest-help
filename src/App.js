3bbaa39e4bb3af3a02cd46b112802a4e75ab15f3
gborh1 <gborh1@gmail.com>
2023-03-15 11:03:53 -0700

    added freshchat widget & pacwest logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
+import pacwest from './PacificWest_Favicon_32x32.svg';
 import './App.css';
+import { Freshchat } from 'reactjs-freshchat';
+import 'reactjs-freshchat/dist/index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={pacwest} className="App-logo" alt="logo" />
+				<p>Use the widget in the lower right hand corner for help!</p>
+				<div>
+					<span>Brought to you by </span>
+					<a
+						className="App-link"
+						href="https://www.pacificwestinc.com/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						PacificWest
+					</a>
+				</div>
+			</header>
+			<Freshchat token={'3ad06d40-b4d2-4761-a344-5519343d7edb'} />
+		</div>
+	);
 }
 
 export default App;
